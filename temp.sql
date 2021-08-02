b0153c92fac5f2ab9a535cbec8673d52ffe5c8b8
Hamza Munaf <46735474+hamzamunaf@users.noreply.github.com>
2021-08-02 09:35:59 -0700

    Update temp.sql

--- temp.sql
+++ temp.sql
@@ -1,28 +1,20 @@
-/*Some Syntax error*/
-CREATE FUNCTION PayLevel(Socialsn INT(9))
-RETURNS VARCHAR(20)
-BEGIN
-  DECLARE Avgs INT(7), Slevel VARCHAR(20);
-  SELECT AVG(EMPLOYEE.Salary) INTO Avgs FROM EMPLOYEE
-  WHERE EMPLOYEE.Dno = DEPARTMENT.Dnumber;
-  AND EMPLOYEE.Ssn = Socialsn
-  IF (EMPLOYEE.Salary > Avgs) THEN RETURN Slevel = "Above Average";
-  ELSE IF (EMPLOYEE.Salary < Avgs) THEN RETURN Slevel = "Below Average";
-  ELSE IF (EMPLOYEE.Salary = Avgs) THEN RETURN Slevel = "Average";
-  END IF;
-END;
-
-/*Does't diplay averageson thewebsiteonly on phpmyadmin*/
-
-BEGIN
-   INSERT INTO
-      DEPT_STATS
-      SELECT
-         Dno,
-         COUNT(Ssn),
-         AVG(Salary)
-      FROM
-         EMPLOYEE
-      GROUP BY
-         Dno;
-END
+CREATE FUNCTION PayLevel(Socialsn INT(9)) RETURNS VARCHAR(20) BEGIN DECLARE Avgs INT(7);DECLARE Slevel VARCHAR(20);
+SELECT
+  AVG(EMPLOYEE.Salary) INTO Avgs
+FROM
+  EMPLOYEE
+WHERE
+  EMPLOYEE.Dno = DEPARTMENT.Dnumber;
+  AND EMPLOYEE.Ssn = Socialsn IF (EMPLOYEE.Salary > Avgs) THEN RETURN Slevel = "Above Average";ELSE IF (EMPLOYEE.Salary < Avgs) THEN RETURN Slevel = "Below Average";ELSE IF (EMPLOYEE.Salary = Avgs) THEN RETURN Slevel = "Average";END IF;END;
+  /*Does't diplay averageson thewebsiteonly on phpmyadmin*/
+  BEGIN
+INSERT INTO
+  DEPT_STATS
+SELECT
+  Dno,
+  COUNT(Ssn),
+  AVG(Salary)
+FROM
+  EMPLOYEE
+GROUP BY
+  Dno;END
